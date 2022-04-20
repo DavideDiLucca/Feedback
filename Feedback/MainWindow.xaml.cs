ca4259d615adac8403576dd3a1ec39fed3665507
Davide Di Lucca <dilucca.davide@leanexperiencefactory.com>
2022-04-20 16:21:26 +0200

    Add project files.

---
+++ Feedback/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Feedback
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string host = @"139.162.174.212";
+        string username = "lefftp";
+        string password = "wic@L3F";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            OpenLink();
+        }
+
+        private void OpenLink()
+        {
+            string feedbackDirectory = @"/home/lefftp/Feedback/link.txt";
+            using (var sftp = new SftpClient(host, username, password))
+            {
+                sftp.Connect();
+
+                try
+                {
+                    using (var remoteFileStream = sftp.OpenRead(feedbackDirectory))
+                    {
+                        using (var reader = new StreamReader(remoteFileStream))
+                        {
+                            string link = reader.ReadLine();
+                            System.Diagnostics.Process.Start(link);
+                            this.Close();
+                            //MessageBox.Show(link);
+                        }
+                    }
+                }
+                finally
+                {
+                    sftp.Disconnect();
+                }
+            }
+        }
+    }
+}
